9b55db97e304d5c84d9433f84bae4abf18f08af0
Vchebrov <64073599+Vchebrov@users.noreply.github.com>
2020-04-23 01:04:33 +0300

    Update Main.py

--- skillfactory/rdsmodule_0/sf_module_0/Main.py
+++ skillfactory/rdsmodule_0/sf_module_0/Main.py
@@ -6,73 +6,52 @@
 
 test_list = []
 for i in range(100):
-    
+
     import random
-    
-    # x = int(input('Укажите нижний диапазон:'))
-    # y = int(input('Укажите верхний диапазон:'))
+
+
     x = 0
-    y = 100 # Задай верхнюю границу диапазона
-    n = random.randint(x,y)
+    y = 100  # Задай верхнюю границу диапазона
+    n = random.randint(x, y)
     print('Угадай число', n)
-    
+
     c = y - x
     count = 0
-    while count!= 100:
+    while count != 100:
         z = c
-        i = int(z/2)
-#         print(i,x,y)
+        i = int(z / 2) # начало бинарного поиска
+
         count += 1
         if i > n:
-#             print('more')
+
             y = i
             c = y - x
         elif i < n:
-#             print('less')
+
             x = i
             c = x + y
         elif i == n:
-            print('Вы угадали число {} с {} попыток'.format(n,count))
+            print('Вы угадали число {} с {} попыток'.format(n, count))
             break
         test_list.append(count)
 print('Final test results', test_list)
 print('Максимальное количество попыток', max(test_list))
 print('Минимальное количество попыток', min(test_list))
-print('Среднее количество попыток', sum(test_list)/len(test_list))
+print('Среднее количество попыток', sum(test_list) / len(test_list))
+
+# In[ ]:
 
 
 # In[ ]:
 
 
-
+# In[ ]:
 
 
 # In[ ]:
 
 
-
-
-
 # In[ ]:
 
 
-
-
-
 # In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
